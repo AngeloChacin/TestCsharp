8bf18ee1bf1b8459fea44ac755d198f9dc037202
unknown <angelochacin@gmailcom>
2020-03-02 08:04:51 -0300

    Primera Version

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string o = "Angelo";
+            Console.WriteLine("My name: {0}", o);
+        }
+    }
+}
