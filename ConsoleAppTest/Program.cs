9d607558de7985564881059206a8469ecd2a7058
Angelo Chacin <angelochacin@gmail.com>
2020-03-05 00:29:23 -0300

    Segundo Cambio

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -7,8 +7,13 @@ namespace ConsoleAppTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string o = "Angelo";
-            Console.WriteLine("My name: {0}", o);
+            string name = "Angelo J.";
+            string lastname = "Chacin";
+
+            Console.WriteLine("TRABAJANDO CON CADENAS");
+            Console.WriteLine("My name: {0}", name);
+            Console.WriteLine(String.Format("Mi nombre es {0}", name));
+            Console.WriteLine($"My name full is: { name } { lastname }.");
         }
     }
 }
